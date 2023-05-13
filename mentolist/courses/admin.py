964d5a2ef78959dfe752d0bbf756f18177be7e6b
Николай Слесарев <kolansers@yandex.ru>
2023-05-13 23:49:33 +0300

    реализована админка для курсов, для всех админок добавлено превью изображения

--- mentolist/courses/admin.py
+++ mentolist/courses/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Question, Category
+from .models import Question, Category, Course, HIGH, AVERAGE, LOW
+
+
+class ImageDisplay(admin.ModelAdmin):
+    def preview_image(self, obj):
+        return mark_safe(
+            f'<img src="{obj.image.url}" style="max-height: 100px;">'
+        )
+    preview_image.short_description = 'Изображение'
+
+    class Meta:
+        abstract = True
 
 
 @admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(ImageDisplay):
     """Админка вопроса."""
     list_display = (
         'number',
@@ -20,20 +31,16 @@ class QuestionAdmin(admin.ModelAdmin):
     )
     readonly_fields = ('preview_image', )
 
-    def preview_image(self, obj):
-        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px;">')
-    preview_image.short_description = 'Фото к вопросу'
-
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(ImageDisplay):
     """Админка категорий."""
     list_display = (
         'title',
         'description',
         'preview_image',
     )
-    search_fields = ('name', 'description', )
+    search_fields = ('title', 'description', )
     fields = (
         'title',
         'description',
@@ -42,6 +49,39 @@ class CategoryAdmin(admin.ModelAdmin):
     )
     readonly_fields = ('preview_image', )
 
-    def preview_image(self, obj):
-        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px;">')
-    preview_image.short_description = 'Фото категории'
+
+@admin.register(Course)
+class CourseAdmin(ImageDisplay):
+    """Админка курсов."""
+    list_display = (
+        'title',
+        'category',
+        'time',
+        'color_level',
+        'description',
+        'audio',
+        'video',
+        'preview_image',
+        'is_audio_course',
+        'is_video_course'
+    )
+    search_fields = ('title', 'description', )
+    list_filter = (
+        'category',
+        'level',
+    )
+
+    def color_level(self, obj):
+        level_color = {
+            HIGH: 'Tomato',
+            AVERAGE: 'Orange',
+            LOW: 'SpringGreen'
+        }
+        return mark_safe(
+            (
+                '<div style="width:100%%; height:100%%; '
+                f'background-color:{level_color[obj.level]};">'
+                f'{obj.get_level_display()}</div>'
+            )
+        )
+    color_level.short_description = 'Сложность курса'
