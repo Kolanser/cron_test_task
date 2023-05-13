73724d6b9b22a8d77f5f223924c801031df5eab6
Николай Слесарев <kolansers@yandex.ru>
2023-05-13 16:10:57 +0300

    добавлен доступ к медиа и статике при разработке, добавлены админки для вопросов и категорий

--- mentolist/courses/admin.py
+++ mentolist/courses/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Question, Category
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """Админка вопроса."""
+    list_display = (
+        'number',
+        'text',
+        'preview_image',
+    )
+    search_fields = ('text', )
+    fields = (
+        'number',
+        'text',
+        'image',
+        'preview_image',
+    )
+    readonly_fields = ('preview_image', )
+
+    def preview_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px;">')
+    preview_image.short_description = 'Фото к вопросу'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админка категорий."""
+    list_display = (
+        'title',
+        'description',
+        'preview_image',
+    )
+    search_fields = ('name', 'description', )
+    fields = (
+        'title',
+        'description',
+        'image',
+        'preview_image',
+    )
+    readonly_fields = ('preview_image', )
+
+    def preview_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 100px;">')
+    preview_image.short_description = 'Фото категории'
